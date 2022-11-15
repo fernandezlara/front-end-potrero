6cc62c0cc630f910914377773d3d268f2df0142f
Lara Fernandez <73143139+fernandezlara@users.noreply.github.com>
2022-11-14 22:09:10 -0300

    Actualizacion - JS

---
+++ js/javascript.js
@@ -0,0 +1,22 @@
+
+function temaOscuro(){
+    document.body.style.backgroundColor="#402C25";
+    document.getElementById("tit-ppal").style.backgroundColor="beige";
+    document.getElementById("tit-receta").style.color="#402C25";
+    document.getElementById("tit2").style.color="beige";
+    document.getElementById("tit3").style.color="beige";
+    document.getElementById("tit4").style.color="beige";
+    document.getElementById("lista1").style.color="#C7B8AD";
+    document.getElementById("lista2").style.color="#C7B8AD";
+    document.getElementById("lista3").style.color="#C7B8AD";
+    document.getElementById("text1").style.color="#C7B8AD";
+    document.getElementById("text2").style.color="#C7B8AD";
+    document.getElementById("text3").style.color="#C7B8AD";
+    document.getElementById("text4").style.color="#C7B8AD";
+}
+
+
+
+function infoFooter(){
+    alert("Lara Fernandez - Potrero Digital")
+}
